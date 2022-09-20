b61007f97da6f803c50f1623249ecf70c0c7bb01
Konstantin Naumov <garfeeeld@mail.ru>
2022-09-20 13:26:01 +0300

    6 Роутинг Code splitting Lazy & 7 Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Counter from "./components/Counter";
-import './index.scss'
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+import {ManePageAsync} from "./pages/MainPage/ManePageAsync";
+import './styles/index.scss'
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className='app'>
-            WORK!!!!
-            <Counter/>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>THEME</button>
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О сайте</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                    <Route path='/' element={<ManePageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/ManePageAsync.tsx
@@ -0,0 +1,3 @@
+import {lazy} from "react";
+
+export const ManePageAsync = lazy(() => import('./MainPage'))

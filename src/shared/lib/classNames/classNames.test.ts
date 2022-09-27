7ec146fc2e3ec6b4ca14bd50d290d624f4bc741b
Konstantin Naumov <garfeeeld@mail.ru>
2022-09-27 15:58:15 +0300

    20 Тестовая среда. Настраиваем Jest. Пишем первый тест

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass additional class1';
+        expect(classNames('someClass', {}, ['additional', 'class1']))
+            .toBe(expected);
+    });
+    test('with additional & mods', () => {
+        const expected = 'someClass additional class1 mods scroll';
+        expect(classNames(
+            'someClass',
+            { mods: true, scroll: true },
+            ['additional', 'class1'],
+        ))
+            .toBe(expected);
+    });
+    test('with additional & mods false', () => {
+        const expected = 'someClass additional class1 mods';
+        expect(classNames(
+            'someClass',
+            { mods: true, scroll: false },
+            ['additional', 'class1'],
+        ))
+            .toBe(expected);
+    });
+    test('with additional & mods undefined', () => {
+        const expected = 'someClass additional class1 mods';
+        expect(classNames(
+            'someClass',
+            { mods: true, scroll: undefined },
+            ['additional', 'class1'],
+        ))
+            .toBe(expected);
+    });
+});

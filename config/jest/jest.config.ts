b6601fa8db8a6c8df18ade0875375c8fd1600175
Konstantin Naumov <garfeeeld@mail.ru>
2022-09-28 15:52:20 +0300

    24 wip

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,16 +4,6 @@
  */
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\Zver\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
